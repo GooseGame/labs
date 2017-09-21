ecc05ca7e3e3048403b2b003ed873c1dc086da5d
Goose <tokarinze@gmail.com>
2017-09-21 08:37:26 +0300

    added animation program

---
+++ 00/main.cpp
@@ -0,0 +1,18 @@
+int main()
+{
+    for (int num = 1; num <= 100; num += 1)
+    {
+        if (num % 3 == v0)
+        {
+            std::cout << "Fizz" << std::endl;
+        }
+        else if (num % 5 == 0)
+        {
+            std::cout << "Buzz" << std::endl;
+        }
+        else
+        {
+            std::cout << num << std::endl;
+        }
+    }
+}
